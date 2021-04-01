55952c80f1e6bdb6b1d8965ea80e93ef7a2251e8
Kevin Bradley <kevin.w.bradley@me.com>
2021-04-01 15:16:33 -0700

    rename to GuardianConnect

---
+++ GuardianConnect/GuardianConnect.h
@@ -0,0 +1,19 @@
+//
+//  GuardianConnect.h
+//  GuardianConnect
+//
+//  Created by Kevin Bradley on 3/30/21.
+//  Copyright © 2021 Sudo Security Group Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GuardianConnect.
+FOUNDATION_EXPORT double GuardianConnectVersionNumber;
+
+//! Project version string for GuardianConnect.
+FOUNDATION_EXPORT const unsigned char GuardianConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GuardianConnect/PublicHeader.h>
+
+
