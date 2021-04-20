64edbc6d63493a849034dad10f0688a606ead1a3
Kevin Bradley <kevin.w.bradley@me.com>
2021-04-20 10:01:08 -0700

    updated script after running it through shellcheck to fix globbing and legacy scripting errors

--- build_ios_framework.sh
+++ build_ios_framework.sh
@@ -8,28 +8,28 @@ xcodebuild -sdk iphonesimulator
 xcodebuild -sdk iphoneos
 
 # keep track of our current working directory
-pwd=`pwd`
+pwd=$(pwd)
 
 # change to the release-iphoneos folder to get the name of the framework (this is to make this script more universal)
 
-pushd build/Release-iphoneos
+pushd build/Release-iphoneos || exit
 
 # find the name of the framework, in our case 'GuardianConnect'
 
-for i in `find * -name "*.framework"`; do
+for i in $(find * -name "*.framework"); do
 
     name=${i%\.*}
-    echo $name
+    echo "$name"
 
 done
 
 # remove the old copy of the xcframework if it already exists
 
-rm -rf ../../$name.xcframework
+rm -rf ../../"$name".xcframework
 
 # pop back to the GuardianConnect folder
 
-popd
+popd || exit
 
 # create variables for the path to each respective framework
 
@@ -39,7 +39,7 @@ sim_fwpath=$pwd/build/Release-iphonesimulator/$name.framework
 
 # create the xcframework
 
-xcodebuild -create-xcframework -framework $ios_fwpath -framework $sim_fwpath -output $name.xcframework
+xcodebuild -create-xcframework -framework "$ios_fwpath" -framework "$sim_fwpath" -output "$name".xcframework
 
 
 
