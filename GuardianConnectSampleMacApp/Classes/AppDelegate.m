0a092f42338cca031e81d5212286e530e3e89f13
Kevin Bradley <kevin.w.bradley@me.com>
2021-04-25 03:58:27 -0700

    make more screen size agnostic

--- GuardianConnectSampleMacApp/Classes/AppDelegate.m
+++ GuardianConnectSampleMacApp/Classes/AppDelegate.m
@@ -508,7 +508,7 @@ uint64_t nowAbsoluteNanoseconds(void) {
 - (void)toggleExpandedManually:(BOOL)manually {
     NSRect screenFrame = [[NSScreen mainScreen] frame];
     NSRect windowFrame = self.alertsWindow.frame;
-    CGFloat padding = 20;
+    CGFloat padding = 40;
     if (_expanded){
         windowFrame.origin.x = screenFrame.size.width - 330 - padding;//1590;
         windowFrame.origin.y = screenFrame.size.height - 200;//1000;
