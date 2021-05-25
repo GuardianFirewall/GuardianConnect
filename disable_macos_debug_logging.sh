6d73270d700fa585d9e4861d95ff16608344d96c
Kevin Bradley <kevin.w.bradley@me.com>
2021-05-25 08:18:57 -0700

    added comments to the script files on what they do

--- disable_macos_debug_logging.sh
+++ disable_macos_debug_logging.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# This is used to undo the changes made by enable_macos_debug_logging.sh and will turn off file / verbose logging of the NetworkExtension framework (framework used for VPN access)
+
 sudo defaults delete /Library/Preferences/com.apple.networkextension.control.plist LogToFile
 sudo defaults delete /Library/Preferences/com.apple.networkextension.control.plist LogLevel
