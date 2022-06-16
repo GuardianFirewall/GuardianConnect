71b7b45f223265cdb9284ef7ba196610f63b1e3f
Constantin Jacob <cjacob@fastmail.com>
2022-06-16 17:28:49 +0200

    Add Swift Package Manager compatible file pointing to the binary release in the appropriate Github release

---
+++ Package.swift
@@ -0,0 +1,22 @@
+//
+//  Package.swift
+//  GuardianConnect
+//
+// swift-tools-version:5.3
+//
+
+import PackageDescription
+
+let package = Package(
+	name: "GuardianConnect",
+	platforms: [
+		.macOS(.v10.15),
+		.iOS(.v13)
+	],
+	products: [
+		.library(name: "GuardianConnect", targets: ["GuardianConnect", "GuardianConnectMac"])
+	],
+	targets: [
+		.binaryTarget(name: "GuardianConnect", url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.5-dev/GuardianConnect.xcframework.zip")
+	]
+)
