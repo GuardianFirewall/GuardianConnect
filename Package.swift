57908438533df4edfcd65aedcaadad2f26210c18
Constantin Jacob <cjacob@fastmail.com>
2022-08-05 17:44:22 +0200

    Fix SwiftPM version format

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "GuardianConnect",
-			url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.7/GuardianConnect.xcframework.zip",
-			checksum: "bbec6dd3e1b336c5616743787b1fdffdc644aaeef0d4de924754af9c0745d5a8"
+			url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.7.0/GuardianConnect.xcframework.zip",
+			checksum: "7898565554ba01afd0ded62c9fca2ecd231fb79645e33a757bcc15d4f0782c5a"
 		)
 	]
 )
