9e8c72912189670e4f865cc13cfcd4cdd6e994b7
Constantin Jacob <cjacob@fastmail.com>
2022-08-30 15:42:38 +0200

    Prepare 1.7.2 SPM release

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "GuardianConnect",
-			url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.7.1/GuardianConnect.xcframework.zip",
-			checksum: "26361a166f16516aa42fb99614c9f88a7a97e04bf2214a87c6ea180a61d01593"
+			url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.7.2/GuardianConnect.xcframework.zip",
+			checksum: "3c34c3dc3858d423474cf4994a4a68c0c0644a7fb0a386719c5e96176565763a"
 		)
 	]
 )
