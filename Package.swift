8ae0dcd2fddaea154cc0e3fb7e7daf7a677ddf6b
Constantin Jacob <cj@tzeejay.com>
2023-03-08 12:20:32 +0100

    Update SPM module

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "GuardianConnect",
-			url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.8.0-dev-5/GuardianConnect.xcframework.zip",
-			checksum: "674749bf0d7ae4e5766eb7239d97b86c5d3f26adb68cdb67a049a68749cf67ab"
+			url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.8.0-dev-6/GuardianConnect.xcframework.zip",
+			checksum: "755a4f5b1cd30f6a50e635f5b9ce8aa0559618a42ad6717860c39cec100e342c"
 		)
 	]
 )
