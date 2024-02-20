a020cc22471dd48a739eac68454ecc282fb54c62
Constantin Jacob <cj@tzeejay.com>
2024-02-20 12:08:07 +0100

    Bump SPM package defintion for new dev-2 build

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "GuardianConnect",
-			url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.8.7-dev-1/GuardianConnect.xcframework.zip",
-			checksum: "d83d9f8c6f4ecd5a33704b718a9064567b1d64aba7d0940156e18e653c4af377"
+			url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.8.7-dev-2/GuardianConnect.xcframework.zip",
+			checksum: "a0d1f34dcb48937efd97b90016c53026c74bd43df8b7a25b7cacb6777d27627d"
 		)
 	]
 )
