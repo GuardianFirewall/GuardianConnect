263cb0936ca4d8f3195a57483c13384f0046b0d4
Constantin Jacob <cj@tzeejay.com>
2026-02-20 09:50:37 +0100

    Remove call out back into GRDVPNHelper if we already have that value locally & update variable names

--- GuardianConnect/Classes/API/GRDServerManager.m
+++ GuardianConnect/Classes/API/GRDServerManager.m
@@ -252,32 +252,32 @@
 		}
 		
 		NSArray *regions = [GRDRegion regionsFromTimezones:items];
-		if (self.returnAutomaticForRegionPrecisionCountry == YES && [[[GRDVPNHelper sharedInstance] regionPrecision] isEqualToString:kGRDRegionPrecisionCityByCountry] == YES) {
+		if (self.returnAutomaticForRegionPrecisionCountry == YES && [self.regionPrecision isEqualToString:kGRDRegionPrecisionCityByCountry] == YES) {
 			NSMutableArray *tmpRegions = [NSMutableArray new];
 			
 			for (GRDRegion *region in regions) {
 				if ([region.cities count] > 1) {
 					NSMutableArray *citiesCopy = [region.cities mutableCopy];
-					GRDRegion *optimal = [GRDRegion new];
-					[optimal setContinent:region.continent];
-					[optimal setCountry:region.country];
-					[optimal setCountryISOCode:region.countryISOCode];
-					[optimal setRegionName:region.regionName];
-					[optimal setIsAutomatic:NO];
-					[optimal setDisplayName:[region.displayName stringByAppendingString:NSLocalizedString(@" - Automatic", nil)]];
+					GRDRegion *countryAutomatic = [GRDRegion new];
+					[countryAutomatic setContinent:region.continent];
+					[countryAutomatic setCountry:region.country];
+					[countryAutomatic setCountryISOCode:region.countryISOCode];
+					[countryAutomatic setRegionName:region.regionName];
+					[countryAutomatic setIsAutomatic:NO];
+					[countryAutomatic setDisplayName:[region.displayName stringByAppendingString:NSLocalizedString(@" - Automatic", nil)]];
 					//
 					// Note from CJ 2026-02-05
 					// Lock the region precision to 'country' here in order to
 					// ensure that we always select from all servers from
 					// any given region with more than one city
-					[optimal setRegionPrecision:kGRDRegionPrecisionCountry];
-					[optimal setLatitude:region.latitude];
-					[optimal setLongitude:region.longitude];
-					[optimal setServerCount:region.serverCount];
-					[optimal setSmartRoutingProxyState:region.smartRoutingProxyState];
-					[optimal setSmartRoutingProxyServers:region.smartRoutingProxyServers];
-					[optimal setTimeZoneName:region.timeZoneName];
-					[citiesCopy insertObject:optimal atIndex:0];
+					[countryAutomatic setRegionPrecision:kGRDRegionPrecisionCountry];
+					[countryAutomatic setLatitude:region.latitude];
+					[countryAutomatic setLongitude:region.longitude];
+					[countryAutomatic setServerCount:region.serverCount];
+					[countryAutomatic setSmartRoutingProxyState:region.smartRoutingProxyState];
+					[countryAutomatic setSmartRoutingProxyServers:region.smartRoutingProxyServers];
+					[countryAutomatic setTimeZoneName:region.timeZoneName];
+					[citiesCopy insertObject:countryAutomatic atIndex:0];
 					[region setCities:citiesCopy];
 				}
 				[tmpRegions addObject:region];
